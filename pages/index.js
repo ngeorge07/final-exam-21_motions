9795313c014cbce0e3ca89125b167c6388e9fd49
George <georgenico9@gmail.com>
2022-12-14 11:41:38 +0100

    fix: center team members on mobile

--- pages/index.js
+++ pages/index.js
@@ -57,9 +57,7 @@ export default function Home() {
           </Text>
         </MainSection>
 
-        <MainSection>
-          <TeamWrapper />
-        </MainSection>
+        <TeamWrapper />
       </main>
 
       <Footer />
